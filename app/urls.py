53b8d51e3e834bb244401d266f80b96dc139ba7d
DostoevskiyRiuui <Hobbycool9@gmail.com>
2026-07-15 13:37:42 +0500

    CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import ProductView
+from rest_framework.routers import DefaultRouter
+
 urlpatterns = [
-    path("", ProductView.as_view({'post':'create'})),
+    path("products/", ProductView.as_view({'get':'list'})),
+    path("products/create/", ProductView.as_view({'post':'create'})),
+    path("products/update/<int:pk>/", ProductView.as_view({'put':'update', 'patch':'partial_update'})),
+    path("products/delete/<int:pk>/", ProductView.as_view({'delete':'destroy'})),
 ]
